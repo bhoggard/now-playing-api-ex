0b75de593f19ddb42be2675003e653d279e850ad
Barry Hoggard <barry@hoggard.org>
2018-12-30 20:27:58 -0500

    set port via env var in production

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
-config :now_playing_api, port: 80
+port =
+case System.get_env("PORT") do
+  port when is_binary(port) -> String.to_integer(port)
+  nil -> 80 # default port
+end
+
+config :my_app, port: port
