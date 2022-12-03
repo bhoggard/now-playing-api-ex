dfd082ca3a065de335eed4dbfb044c447b467451
Barry Hoggard <barry@hoggard.org>
2022-12-03 14:09:29 -0500

    set elixir and erlang versions

--- config/prod.exs
+++ config/prod.exs
@@ -10,5 +10,4 @@ config :now_playing_api, port: port
 
 config :now_playing_api, NowPlayingApi.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-  server: true
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
